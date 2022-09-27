f2bc9c2ca2a77ac8c5189b94f62ccb8cce953d61
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 16:05:16 +0200

    #2-Iteration & #2 Style Fix

--- Javascript/neoland-basic-algorithms/#1-Iteration.js
+++ Javascript/neoland-basic-algorithms/#1-Iteration.js
@@ -1,12 +1,16 @@
 // 1
+
 let myFavoriteHero = 'Hulk';
 
 // 1.2
+
 let x = 50;
 
 // 1.3
+
 let h = 5;
 let y = 10;
 
 // 1.4
+
 let z = h + y;
---
+++ Javascript/neoland-basic-algorithms/#2-Iteration.js
@@ -0,0 +1,24 @@
+// 1.1
+
+const character = { name: "Jack Sparrow", age: 25 };
+
+//1.2
+
+firstName = "Jon";
+lastName = "Snow";
+age = 24;
+
+console.log(`'Soy ${firstName} ${lastName}, tengo 24 años y me gustan los lobos.'`);
+
+// 1.3
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+
+console.log(toy1.price + toy2.price);
+
+// 1.4
+
+let globalBasePrice = 25000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: basePrice + globalPrice};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: basePrice + globalPrice};
