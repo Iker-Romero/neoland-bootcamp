9880d12619faec2d3bddc1d780d71dc558a8820c
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 17:33:08 +0200

    Functions #2-Iteration.

---
+++ Javascript/Exercises/neoland-functions/#2-Iteration.js
@@ -0,0 +1,13 @@
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+function findLongestWord(param) {
+  let longest = "";
+  for (let i = 0; i <= param.length - 1; i++) {
+    if (param[i].length > longest.length) {
+        longest = param[i];
+    }
+  }
+  return longest;
+}
+
+console.log(findLongestWord(avengers));
