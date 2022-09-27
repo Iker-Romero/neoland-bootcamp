126f1b050326998c3c420289f5d340ad19ec86bf
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 17:59:08 +0200

    Functions #4-Iteration.

--- Javascript/Exercises/neoland-functions/#3-Iteration.js
+++ Javascript/Exercises/neoland-functions/#3-Iteration.js
@@ -1,7 +1,7 @@
 const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumAll(param) {
-    let total = 0;
+  let total = 0;
   for (let i = 0; i <= param.length - 1; i++) {
     total += param[i];
   }
---
+++ Javascript/Exercises/neoland-functions/#4-Iteration.js
@@ -0,0 +1,11 @@
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+
+function average(param) {
+  total = 0;
+  for (let i = 0; i <= param.length - 1; i++) {
+    total += param[i];
+  }
+  return total / param.length;
+}
+
+console.log(average(numbers));
