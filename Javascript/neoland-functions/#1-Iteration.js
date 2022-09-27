c8c842538a6b41894b6bc9178830eb2c248e1712
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 17:01:52 +0200

    #1-Iteration

---
+++ Javascript/neoland-functions/#1-Iteration.js
@@ -0,0 +1,9 @@
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        return numberOne;
+    } else if (numberOne < numberTwo) {
+        return numberTwo;
+    } else {
+        return console.log('The numbers are equal.');
+    }
+}
