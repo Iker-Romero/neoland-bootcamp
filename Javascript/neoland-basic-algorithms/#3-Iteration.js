1ef25d3abec1e7daa9c4b9f1504d2e6c686d8a8b
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 16:14:23 +0200

    #3-Iteration

---
+++ Javascript/neoland-basic-algorithms/#3-Iteration.js
@@ -0,0 +1,25 @@
+// 1.1
+
+console.log(10 * 5);
+
+// 1.2
+
+console.log(10 / 2);
+
+// 1.3
+
+console.log(15 % 9);
+
+// 1.4
+
+let p = 10;
+let j = 5;
+
+let o = p + j;
+
+// 1.5
+
+let c = 10;
+let m = 5;
+
+let i = c * m;
