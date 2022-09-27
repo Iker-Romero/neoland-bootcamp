894d24ca30c9321ab055cfa3d9cda73a6228a2c4
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 16:54:33 +0200

    #6-Iteration

---
+++ Javascript/neoland-basic-algorithms/#6-Iteration.js
@@ -0,0 +1,23 @@
+// 1.1
+
+for (let i = 1; i <= 9; i++) {
+    console.log(i);
+}
+
+// 1.2
+
+for (let i = 1; i <= 9; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// 1.3
+
+for (let i = 1; i <= 10; i++) {
+    if (i === 10) {
+        console.log('Dormido!');
+    } else {
+        console.log('Intentando dormir');
+    }
+}
