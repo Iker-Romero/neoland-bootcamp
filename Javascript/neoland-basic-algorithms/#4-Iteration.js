8f68c9920635335a4dc904c9bee657a0bf76f0ca
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 16:30:27 +0200

    #4-Iteration. 1.5 and 1.5 can't be comopleted yet.

---
+++ Javascript/neoland-basic-algorithms/#4-Iteration.js
@@ -0,0 +1,32 @@
+// 1.1
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+// 1.2
+
+avengers[0] = 'IRONMAN';
+
+// 1.3
+
+console.log(avengers.length);
+
+// 1.4
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+rickAndMortyCharacters.push('Morty', 'Summer');
+
+// 1.5 (TODAVÍA NO)
+
+rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters.pop();
+
+console.log(rickAndMortyCharacters[0]);
+
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+// 1.6 (TODAVÍA NO)
+
