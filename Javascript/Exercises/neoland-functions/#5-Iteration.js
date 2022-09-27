263500536779941b14378273a9786d837f32e7ee
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 21:22:18 +0200

    Functions #5-Iteration.

---
+++ Javascript/Exercises/neoland-functions/#5-Iteration.js
@@ -0,0 +1,15 @@
+const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+
+function averageWord(param) {
+  let total = 0;
+  for (let i = 0; i <= param.length - 1; i++) {
+    if (typeof param[i] === "number") {
+      total += param[i];
+    } else if (typeof param[i] === "string") {
+      total += param[i].length;
+    }
+  }
+  return total / param.length;
+}
+
+console.log(averageWord(mixedElements));
