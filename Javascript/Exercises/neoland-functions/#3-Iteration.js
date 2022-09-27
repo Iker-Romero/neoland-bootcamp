9194662dd4d5fc5dd0f874ddf8d03c7d3bd26801
Iker-Romero <ikersoftdev@gmail.com>
2022-09-27 17:41:30 +0200

    Functions #3-Iteration.

---
+++ Javascript/Exercises/neoland-functions/#3-Iteration.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+    let total = 0;
+  for (let i = 0; i <= param.length - 1; i++) {
+    total += param[i];
+  }
+  return total;
+}
+
+console.log(sumAll(numbers));
