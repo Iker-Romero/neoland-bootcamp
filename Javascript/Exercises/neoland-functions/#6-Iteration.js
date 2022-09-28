df2fcee143876dfe51609ba829e7430cfe4f012d
Iker-Romero <ikersoftdev@gmail.com>
2022-09-28 19:08:29 +0200

    Functions #6-Iteration.

--- Javascript/Exercises/neoland-functions/#6-Iteration.js
+++ Javascript/Exercises/neoland-functions/#6-Iteration.js
@@ -15,20 +15,10 @@ const duplicates = [
 function removeDuplicates(param) {
   for (let i = 0; i <= param.length - 1; i++) {
     if (i !== param.lastIndexOf(param[i])) {
-      param.splice(i)
+      param.splice(i, 1);
     }
   }
   return param;
 }
 
 console.log(removeDuplicates(duplicates));
-
-// function removeDuplicates(param) {
-//     for (let i = 0; i <= param.length - 1; i++) {
-//         for (let j = i + 1; j <= param.length; i++) {
-//           if (param[i] === param[j]) {
-//             param[j].de
-//           }
-//         }
-//     }
-// }
