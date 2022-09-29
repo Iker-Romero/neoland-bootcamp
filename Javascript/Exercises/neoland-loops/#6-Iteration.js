28b69f74a507ff96f99ac0e11d92c7ce1348d002
Iker-Romero <ikersoftdev@gmail.com>
2022-09-29 18:54:00 +0200

    Loops #6-Iteration con push() en vez de splice().

---
+++ Javascript/Exercises/neoland-loops/#6-Iteration.js
@@ -0,0 +1,20 @@
+const toys = [
+  { id: 5, name: "Buzz MyYear" },
+  { id: 11, name: "Action Woman" },
+  { id: 23, name: "Barbie Man" },
+  { id: 40, name: "El gato con Guantes" },
+  { id: 40, name: "El gato felix" },
+];
+
+newToys = [];
+
+for (const toy of toys) {
+  if (((toys[toys.indexOf(toy)].name).includes("gato")) === false) {
+    // toys.splice(toys.indexOf(toy), 1);
+    array.push(toy);
+  }
+}
+
+console.log(newToys);
+
+console.log(array);
