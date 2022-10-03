03ccf01035b9601390bbe7eb2038e53d8ed5f655
Iker-Romero <ikersoftdev@gmail.com>
2022-10-03 16:48:25 +0200

    ES6 #3-Iteration

---
+++ Javascript/Exercises/neoland-es6/#3-Iteration.js
@@ -0,0 +1,13 @@
+// Iteración #3: Spread Operator
+
+// 3.1 Dado el siguiente array, crea una copia usando spread operators.
+
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+const pointsListCopy = [...pointsList];
+
+// 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+
+const toyCopy = { ...toy };
