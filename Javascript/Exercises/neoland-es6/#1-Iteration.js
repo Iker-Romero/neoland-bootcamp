b7d5ae1d23230d98de4e4a543b868bc8479f4aa2
Iker-Romero <ikersoftdev@gmail.com>
2022-10-03 16:21:08 +0200

    ES6 #1-Iteration

---
+++ Javascript/Exercises/neoland-es6/#1-Iteration.js
@@ -0,0 +1,14 @@
+// Iteración #1: Arrows
+
+//Crea una arrow function que tenga dos parametros a y b y que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre por consola la suma de los dos parametros.
+
+const suma = (a, b) => console.log(a + b);
+
+// 1.1 Ejecuta esta función sin pasar ningún parametro
+suma();
+// 1.2 Ejecuta esta función pasando un solo parametro
+suma(1);
+// 1.3 Ejecuta esta función pasando dos parametros
+suma(1, 4);
+
+
